36878db0d4a6be04b5c102c8fe72e37991343747
Na'im <naimtrader@gmail.com>
2023-09-26 10:23:25 -0400

    Test

---
+++ src/test/java/com/codedifferentlty/labs/partD/POICounterTest.java
@@ -0,0 +1,35 @@
+package com.codedifferentlty.labs.partD;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class POICounterTest {
+    private POICounter poiCounter;
+    @BeforeEach
+    void setUp(){
+        poiCounter = new POICounter();
+    }
+
+    @Test
+    void addToPOITest01 (){
+        poiCounter.addToPOI("Zoo Miami", 2.0);
+        assertEquals(2.0, poiCounter.getPOI().get("Zoo Miami"));
+
+        poiCounter.addToPOI("Zoo Miami", 1.0);
+        assertEquals(3.0,poiCounter.getPOI().get("Zoo Miami"));
+    }
+
+    @Test
+    void removeFromPOITest01(){
+        poiCounter.addToPOI("Zoo Miami", 5.0);
+        assertEquals(5.0, poiCounter.getPOI().get("Zoo Miami"));
+
+        poiCounter.removeFromPOI("Zoo Miami", 1.0);
+        assertEquals(4.0, poiCounter.getPOI().get("Zoo Miami"));
+
+        poiCounter.removeFromPOI("Zoo Miami",4.0);
+        assertEquals(0.0, poiCounter.getPOI().get("Zoo Miami"));
+    }
+}
